0cd6445576f290405aeb700ff0a9e41d681810d3
Emanuele Di Marco <emanuele@cern.ch>
2013-11-22 10:46:39 +0100

    synch with e/g recipe for 42X

--- HiggsAnalysis/HiggsToWW2e/python/electronIdSequence_cff.py
+++ HiggsAnalysis/HiggsToWW2e/python/electronIdSequence_cff.py
@@ -8,11 +8,11 @@ eIdSequence = cms.Sequence( egammaIDLikelihood )
 
 # to compute FastJet rho to correct isolation (note: EtaMax restricted to 2.5)
 from RecoJets.JetProducers.kt4PFJets_cfi import *
-kt6PFJetsForIsolation = kt4PFJets.clone( rParam = 0.6, doRhoFastjet = True )
-kt6PFJetsForIsolation.Rho_EtaMax = cms.double(2.5)
+kt6PFJetsForIsolation = kt4PFJets.clone( rParam = 0.6,
+                                         doRhoFastjet = True,
+                                         doAreaFastjet = cms.bool(True) )
 
-# too much memory used. Choose one
-#kt6PFJetsNoPUForIsolation = kt4PFJets.clone( src = cms.InputTag('pfNoPileUp'), rParam = 0.6, doRhoFastjet = True )
-#kt6PFJetsNoPUForIsolation.Rho_EtaMax = cms.double(2.5)
+kt6PFJetsForIsolation.Rho_EtaMax = cms.double(2.5)
+kt6PFJetsForIsolation.Ghost_EtaMax = cms.double(2.5)
 
 FastjetForIsolation = cms.Sequence( kt6PFJetsForIsolation )

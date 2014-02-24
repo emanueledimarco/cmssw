82ea94493d0a27fac3a7d7cadd9c48022f7ea68b
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-02-24 10:11:03 +0100

    add the time plot

--- RecoLocalCalo/EcalRecAlgos/test/makePlotsToys.cc
+++ RecoLocalCalo/EcalRecAlgos/test/makePlotsToys.cc
@@ -12,6 +12,50 @@
 
 using namespace std;
 
+void makeplotstoysTime(TString dir = "./", TString file = "toyresults.root", 
+		   TString suffix = "") {
+
+  gROOT->SetStyle("Plain");
+  gStyle->SetOptTitle(0);
+  gStyle->SetOptFit(1111111);
+
+  TFile *resfile = TFile::Open(file);
+  TTree *ntp = (TTree*)resfile->Get("dataset");
+
+  TH1D* sigTime_pull = new TH1D("sigTime_pull", " ", 20,  -4., 4.  );
+  TH1D* sigTime_err  = new TH1D("sigTime_err" , " ", 30,  0., 2.);
+
+  ntp->Project("sigTime_pull","(sigIT_Time_mean_0 - sigIT_Time_meangen)/sigIT_Time_meanerr_0","covQual_0==3");
+  ntp->Project("sigTime_err_0","sigIT_Time_mean_0","covQual_0==3");
+
+  // pull distributions, fitted as Gaussians
+  TCanvas* c1 = new TCanvas("c1","c1",500,500);
+  c1->SetFillColor(0);
+  c1->SetBorderMode(0);
+  c1->SetBorderSize(2);
+  c1->SetFrameBorderMode(0);
+  c1->SetFrameBorderMode(0);
+  gStyle->SetOptFit(1);
+  gStyle->SetOptStat(0);
+
+  sigTime_pull->Fit("gaus");
+  sigTime_pull->GetXaxis()->SetTitle("Pull N_{sig IT}");
+  TString pullstring(dir+"/sigTime_pull");
+  pullstring.Append(suffix);
+  pullstring.Append(".pdf");
+  c1->SaveAs(pullstring);
+
+  // error distributions
+  TCanvas* c2 = new TCanvas("c2","c2",500,500);
+  c2->SetFillColor(0);
+  c2->SetBorderMode(0);
+  c2->SetBorderSize(2);
+  c2->SetFrameBorderMode(0);
+  c2->SetFrameBorderMode(0);
+  gStyle->SetOptFit(0);
+  gStyle->SetOptStat(1111111);
+
+}
 
 void makeplotstoys(TString dir = "./", TString file = "toyresults.root", 
 		   TString suffix = "") {
@@ -39,13 +83,7 @@ void makeplotstoys(TString dir = "./", TString file = "toyresults.root",
   TH1D* N_bkgOoT_minus4bx_err  = new TH1D("N_bkgOoT_minus4bx_err",  " ", 200, 0., 20. );
 
 
-  cout << "ntp address = " << ntp << endl;
-
-  cout << "entries = " << ntp->GetEntries() << endl;
   ntp->Project("N_sigIT_pull","(N_sigIT_0 - N_sigITgen)/N_sigITerr_0","covQual_0==3");
-
-  cout << "entries = " << ntp->GetEntries() << endl;
-
   ntp->Project("N_sigIT_err_0","N_sigITerr_0","covQual_0==3");
 
   ntp->Project("N_bkgOoT_minus1bx_pull","(N_bkgOoT_minus1bx_0 - N_bkgOoT_minus1bxgen)/N_bkgOoT_minus1bxerr_0","covQual_0==3");
@@ -60,8 +98,6 @@ void makeplotstoys(TString dir = "./", TString file = "toyresults.root",
   ntp->Project("N_bkgOoT_minus4bx_pull","(N_bkgOoT_minus4bx_0 - N_bkgOoT_minus4bxgen)/N_bkgOoT_minus4bxerr_0","covQual_0==3");
   ntp->Project("N_bkgOoT_minus4bx_err","N_bkgOoT_minus4bxerr_0","covQual_0==3");
 
-  cout << "entries = " << ntp->GetEntries() << endl;
- 
   // pull distributions, fitted as Gaussians
   TCanvas* c1 = new TCanvas("c1","c1",500,500);
   c1->SetFillColor(0);

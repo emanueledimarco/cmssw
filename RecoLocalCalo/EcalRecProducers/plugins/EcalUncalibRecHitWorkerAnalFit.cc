c1df5ed0367731649b794d1b579799793f78eb6c
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-03-05 20:16:45 +0100

    - open here the ROOT file with the shapes - add the configurable parameters on the minAmpl, save of the file, etc

--- RecoLocalCalo/EcalRecProducers/plugins/EcalUncalibRecHitWorkerAnalFit.cc
+++ RecoLocalCalo/EcalRecProducers/plugins/EcalUncalibRecHitWorkerAnalFit.cc
@@ -4,7 +4,7 @@
  *  $Id: EcalUncalibRecHitWorkerAnalFit.cc,v 1.1 2008/12/10 01:52:42 ferriff Exp $
  *  $Date: 2008/12/10 01:52:42 $
  *  $Revision: 1.1 $
- *  \author Shahram Rahatlou, University of Rome & INFN, Sept 2005
+ *  \author E. Di Marco, Caltech
  *
  */
 #include "RecoLocalCalo/EcalRecProducers/plugins/EcalUncalibRecHitWorkerAnalFit.h"
@@ -21,13 +21,9 @@
 
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-//#include "CondFormats/EcalObjects/interface/EcalPedestals.h"
-//#include "CondFormats/DataRecord/interface/EcalPedestalsRcd.h"
 #include "DataFormats/EcalRecHit/interface/EcalUncalibratedRecHit.h"
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
 
-//#include "CLHEP/Matrix/Matrix.h"
-//#include "CLHEP/Matrix/SymMatrix.h"
 #include <vector>
 
 #include "CondFormats/EcalObjects/interface/EcalPedestals.h"
@@ -38,102 +34,144 @@
 #include "CondFormats/DataRecord/interface/EcalGainRatiosRcd.h"
 
 EcalUncalibRecHitWorkerAnalFit::EcalUncalibRecHitWorkerAnalFit(const edm::ParameterSet& ps) :
-        EcalUncalibRecHitWorkerBaseClass( ps )
+  EcalUncalibRecHitWorkerBaseClass( ps )
 {
+  algoEB_.SetMinAmpl( ps.getParameter<double> ("MinAmplBarrel") );
+  algoEE_.SetMinAmpl( ps.getParameter<double> ("MinAmplEndcap") );
+
+  bool dyn_pede = ps.getParameter<bool>("UseDynamicPedestal");
+  algoEB_.SetDynamicPedestal(dyn_pede);
+  algoEE_.SetDynamicPedestal(dyn_pede);
+
+  shapesFileName_ = ps.getUntrackedParameter<std::string>("ShapesFileName");
+
+  savePlot_ = ps.getUntrackedParameter<bool>("SavePlot"); 
+  algoEB_.SavePlot(savePlot_);
+  algoEE_.SavePlot(savePlot_);
 }
 
 
 void
 EcalUncalibRecHitWorkerAnalFit::set(const edm::EventSetup& es)
 {
-        // Gain Ratios
-        LogDebug("EcalUncalibRecHitDebug") << "fetching gainRatios....";
-        es.get<EcalGainRatiosRcd>().get(pRatio);
-        LogDebug("EcalUncalibRecHitDebug") << "done." ;
+  // Gain Ratios
+  LogDebug("EcalUncalibRecHitDebug") << "fetching gainRatios....";
+  es.get<EcalGainRatiosRcd>().get(pRatio);
+  LogDebug("EcalUncalibRecHitDebug") << "done." ;
 
-        // fetch the pedestals from the cond DB via EventSetup
-        LogDebug("EcalUncalibRecHitDebug") << "fetching pedestals....";
-        es.get<EcalPedestalsRcd>().get( pedHandle );
-        LogDebug("EcalUncalibRecHitDebug") << "done." ;
+  // fetch the pedestals from the cond DB via EventSetup
+  LogDebug("EcalUncalibRecHitDebug") << "fetching pedestals....";
+  es.get<EcalPedestalsRcd>().get( pedHandle );
+  LogDebug("EcalUncalibRecHitDebug") << "done." ;
+
+  // fetch the shapes of the in time and out of time from a ROOT file (temporary)
+  TFile *fileTemplates = TFile::Open(shapesFileName_.c_str());
+
+  for( int s=0; s<2; ++s) {
+    const std::string namesector ( 0 == s ? "Barrel" : "Endcap") ;
+    const std::string nameh  ( "Ecal" + namesector + "Shape" ) ;
+    TH1D *shape = (TH1D*)fileTemplates->Get(nameh.c_str());
+    
+    // Get the OOT PU shapes (shifted replicas of the IT one)
+    std::vector<TH1D*> oot_shapes;
+    // replicate it for -10bx +3bx
+    for (int i = -10; i<4; ++i) {
+      if(i==0) continue;
+      char buf[100];
+      if(i>0) sprintf(buf,"%s_oot+%dbx",nameh.c_str(),i);
+      else sprintf(buf,"%s_oot%dbx",nameh.c_str(),i);
+      TH1D *ootshape = (TH1D*)fileTemplates->Get(buf);
+      oot_shapes.push_back(ootshape);
+    }
+    if(s == 0) {
+      algoEB_.SetInTimeShape(shape);
+      algoEB_.SetOutOfTimeShapes(oot_shapes);
+    } else {
+      algoEE_.SetInTimeShape(shape);
+      algoEE_.SetOutOfTimeShapes(oot_shapes);
+    }
+  }
+
 }
 
 bool
 EcalUncalibRecHitWorkerAnalFit::run( const edm::Event& evt,
-                const EcalDigiCollection::const_iterator & itdg,
-                EcalUncalibratedRecHitCollection & result)
+                                     const EcalDigiCollection::const_iterator & itdg,
+                                     EcalUncalibratedRecHitCollection & result)
 {
-        using namespace edm;
+  using namespace edm;
 
-        const EcalGainRatioMap & gainMap = pRatio.product()->getMap(); // map of gain ratios
-        const EcalPedestalsMap & pedMap = pedHandle.product()->getMap(); // map of pedestals
+  const EcalGainRatioMap & gainMap = pRatio.product()->getMap(); // map of gain ratios
+  const EcalPedestalsMap & pedMap = pedHandle.product()->getMap(); // map of pedestals
 
-        EcalPedestalsMapIterator pedIter; // pedestal iterator
-        EcalPedestals::Item aped; // pedestal object for a single xtal
+  EcalPedestalsMapIterator pedIter; // pedestal iterator
+  EcalPedestals::Item aped; // pedestal object for a single xtal
 
-        EcalGainRatioMap::const_iterator gainIter; // gain iterator
-        EcalMGPAGainRatio aGain; // gain object for a single xtal
+  EcalGainRatioMap::const_iterator gainIter; // gain iterator
+  EcalMGPAGainRatio aGain; // gain object for a single xtal
 
-        DetId detid( itdg->id() );
+  DetId detid( itdg->id() );
 
-        // find pedestals for this channel
-        //LogDebug("EcalUncalibRecHitDebug") << "looking up pedestal for crystal: " << itdg->id(); // FIXME
-        pedIter = pedMap.find( detid );
-        if( pedIter != pedMap.end() ) {
-                aped = (*pedIter);
-        } else {
-                edm::LogError("EcalUncalibRecHitWorkerAnalFit") << "error!! could not find pedestals for channel: ";
-                if ( detid.subdetId() == EcalBarrel ) {
-                        edm::LogError("EcalUncalibRecHitWorkerAnalFit") << EBDetId( detid );
-                } else {
-                        edm::LogError("EcalUncalibRecHitWorkerAnalFit") << EEDetId( detid );
-                }
-                edm::LogError("EcalUncalibRecHitWorkerAnalFit") << "\n  no uncalib rechit will be made for this digi!";
-                return false;
-        }
-        double pedVec[3];
-        pedVec[0] = aped.mean_x12;
-        pedVec[1] = aped.mean_x6;
-        pedVec[2] = aped.mean_x1;
+  // find pedestals for this channel
+  //LogDebug("EcalUncalibRecHitDebug") << "looking up pedestal for crystal: " << itdg->id(); // FIXME
+  pedIter = pedMap.find( detid );
+  if( pedIter != pedMap.end() ) {
+    aped = (*pedIter);
+  } else {
+    edm::LogError("EcalUncalibRecHitWorkerAnalFit") << "error!! could not find pedestals for channel: ";
+    if ( detid.subdetId() == EcalBarrel ) {
+      edm::LogError("EcalUncalibRecHitWorkerAnalFit") << EBDetId( detid );
+    } else {
+      edm::LogError("EcalUncalibRecHitWorkerAnalFit") << EEDetId( detid );
+    }
+    edm::LogError("EcalUncalibRecHitWorkerAnalFit") << "\n  no uncalib rechit will be made for this digi!";
+    return false;
+  }
+  double pedVec[3];
+  pedVec[0] = aped.mean_x12;
+  pedVec[1] = aped.mean_x6;
+  pedVec[2] = aped.mean_x1;
 
 
-        // find gain ratios
-        //LogDebug("EcalUncalibRecHitDebug") << "looking up gainRatios for crystal: " << itdg->id(); // FIXME
-        gainIter = gainMap.find( detid );
-        if( gainIter != gainMap.end() ) {
-                aGain = (*gainIter);
-        } else {
-                edm::LogError("EcalUncalibRecHitWorkerAnalFit") << "error!! could not find gain ratios for channel: ";
-                if ( detid.subdetId() == EcalBarrel ) {
-                        edm::LogError("EcalUncalibRecHitWorkerAnalFit") << EBDetId( detid );
-                } else {
-                        edm::LogError("EcalUncalibRecHitWorkerAnalFit") << EEDetId( detid );
-                }
-                edm::LogError("EcalUncalibRecHitWorkerAnalFit") << "\n  no uncalib rechit will be made for this digi!";
-                return false;
-        }
-        double gainRatios[3];
-        gainRatios[0] = 1.;
-        gainRatios[1] = aGain.gain12Over6();
-        gainRatios[2] = aGain.gain6Over1()*aGain.gain12Over6();
+  // find gain ratios
+  //LogDebug("EcalUncalibRecHitDebug") << "looking up gainRatios for crystal: " << itdg->id(); // FIXME
+  gainIter = gainMap.find( detid );
+  if( gainIter != gainMap.end() ) {
+    aGain = (*gainIter);
+  } else {
+    edm::LogError("EcalUncalibRecHitWorkerAnalFit") << "error!! could not find gain ratios for channel: ";
+    if ( detid.subdetId() == EcalBarrel ) {
+      edm::LogError("EcalUncalibRecHitWorkerAnalFit") << EBDetId( detid );
+    } else {
+      edm::LogError("EcalUncalibRecHitWorkerAnalFit") << EEDetId( detid );
+    }
+    edm::LogError("EcalUncalibRecHitWorkerAnalFit") << "\n  no uncalib rechit will be made for this digi!";
+    return false;
+  }
+  double gainRatios[3];
+  gainRatios[0] = 1.;
+  gainRatios[1] = aGain.gain12Over6();
+  gainRatios[2] = aGain.gain6Over1()*aGain.gain12Over6();
 
-        if ( detid.subdetId() == EcalBarrel ) {
-                EcalUncalibratedRecHit aHit = algoEB_.makeRecHit(*itdg, pedVec, gainRatios, 0 ,0);
-                result.push_back( aHit );
-                if(aHit.amplitude()>0.) {
-                        LogDebug("EcalUncalibRecHitInfo") << "EcalUncalibRecHitWorkerAnalFit: processed EBDataFrame with id: "
-                                << EBDetId( detid )
-                                << "\n" << "uncalib rechit amplitude: " << aHit.amplitude();
-                }
-        } else {
-                EcalUncalibratedRecHit aHit = algoEE_.makeRecHit(*itdg, pedVec, gainRatios, 0, 0);
-                result.push_back( aHit );
-                if(aHit.amplitude()>0.) {
-                        LogDebug("EcalUncalibRecHitInfo") << "EcalUncalibRecHitWorkerAnalFit: processed EEDataFrame with id: "
-                                << EEDetId( detid ) << "\n"
-                                << "uncalib rechit amplitude: " << aHit.amplitude();
-                }
-        }
-        return true;
+  if ( detid.subdetId() == EcalBarrel ) {
+
+    EcalUncalibratedRecHit aHit = algoEB_.makeRecHit(*itdg, pedVec, gainRatios, 0 ,0);
+    result.push_back( aHit );
+    if(aHit.amplitude()>0.) {
+      LogDebug("EcalUncalibRecHitInfo") << "EcalUncalibRecHitWorkerAnalFit: processed EBDataFrame with id: "
+                                        << EBDetId( detid )
+                                        << "\n" << "uncalib rechit amplitude: " << aHit.amplitude();
+    }
+  } else {
+    EcalUncalibratedRecHit aHit = algoEE_.makeRecHit(*itdg, pedVec, gainRatios, 0, 0);
+    result.push_back( aHit );
+    if(aHit.amplitude()>0.) {
+      LogDebug("EcalUncalibRecHitInfo") << "EcalUncalibRecHitWorkerAnalFit: processed EEDataFrame with id: "
+                                        << EEDetId( detid ) << "\n"
+                                        << "uncalib rechit amplitude: " << aHit.amplitude();
+    }
+  }
+  return true;
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
--- RecoLocalCalo/EcalRecProducers/python/ecalAnalFitUncalibRecHit_cfi.py
+++ RecoLocalCalo/EcalRecProducers/python/ecalAnalFitUncalibRecHit_cfi.py
@@ -5,6 +5,11 @@ ecalAnalFitUncalibRecHit = cms.EDProducer("EcalUncalibRecHitProducer",
     EBdigiCollection = cms.InputTag("ecalDigis","ebDigis"),
     EEhitCollection = cms.string('EcalUncalibRecHitsEE'),
     EEdigiCollection = cms.InputTag("ecalDigis","eeDigis"),
+    MinAmplEndcap = cms.double(6.0),
+    MinAmplBarrel = cms.double(3.0),
+    UseDynamicPedestal = cms.bool(True),
+    ShapesFileName = cms.untracked.string("RecoLocalCalo/EcalRecAlgos/data/EcalShapes.root"),
+    SavePlot = cms.untracked.bool(True),
     EBhitCollection = cms.string("EcalUncalibRecHitsEB"),
     algo = cms.string("EcalUncalibRecHitWorkerAnalFit")
 )

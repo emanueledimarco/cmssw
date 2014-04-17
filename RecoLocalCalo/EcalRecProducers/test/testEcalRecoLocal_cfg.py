86950d67002036ce65c561296b476d3968e15f48
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-04-17 17:13:22 +0200

    migrate to 71X

--- RecoLocalCalo/EcalRecProducers/test/testEcalRecoLocal_cfg.py
+++ RecoLocalCalo/EcalRecProducers/test/testEcalRecoLocal_cfg.py
@@ -1,7 +1,36 @@
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("testEcalRecoLocal")
 
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("RecoEgamma.EgammaElectronProducers.gsfElectronSequence_cff")
+process.load("Configuration.StandardSequences.DigiToRaw_cff")
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("EventFilter.EcalRawToDigi.EcalUnpackerMapping_cfi")
+process.load("EventFilter.EcalRawToDigi.EcalUnpackerData_cfi")
+
+#### CONFIGURE IT HERE
+isMC = True
+# methods: Global, Weights, AlphaBetaFit, AnalyticFit
+AmplitudeRecoMethod = "Global"
+runOnRAW = False
+runProfiler = False
+#####################
+
+if runProfiler:
+    process.IgProfService = cms.Service("IgProfService",
+                                        reportFirstEvent            = cms.untracked.int32(0),
+                                        reportEventInterval         = cms.untracked.int32(1),
+                                        reportToFileAtPostEvent     = cms.untracked.string("| gzip -c > XXXX.%I.gz")
+                                        )
+
+if isMC:
+    process.ecalEBunpacker.InputLabel = cms.InputTag('rawDataCollector')
+else:
+    process.ecalEBunpacker.InputLabel = cms.InputTag('source')
 
 # get timing service up for profiling
 process.TimerService = cms.Service("TimerService")
@@ -9,81 +38,118 @@ process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
 )
 
-
-# unpacking
-process.load("EventFilter.EcalRawToDigi.EcalUnpackerMapping_cfi")
-process.load("EventFilter.EcalRawToDigi.EcalUnpackerData_cfi")
-
-# load trivial conditions
-process.EcalTrivialConditionRetriever = cms.ESSource("EcalTrivialConditionRetriever",
-                                                         adcToGeVEBConstant = cms.untracked.double(0.035),
-                                                         adcToGeVEEConstant = cms.untracked.double(0.06),
-                                                         pedWeights = cms.untracked.vdouble(0.333, 0.333, 0.333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
-                                                         amplWeights = cms.untracked.vdouble(-0.333, -0.333, -0.333, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0),
-                                                         jittWeights = cms.untracked.vdouble(0.041, 0.041, 0.041, 0.0, 1.325, -0.05, -0.504, -0.502, -0.390, 0.0)
-                                                     )
-
+# get uncalib rechits from global method (weights for ampli, time from ratio, etc)
+import RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi
+process.ecalUncalibHitGlobal = RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi.ecalGlobalUncalibRecHit.clone()
 
 # get uncalibrechits with weights method
 import RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi
 process.ecalUncalibHitWeights = RecoLocalCalo.EcalRecProducers.ecalWeightUncalibRecHit_cfi.ecalWeightUncalibRecHit.clone()
-process.ecalUncalibHitWeights.EBdigiCollection = 'ecalEBunpacker:ebDigis'
-process.ecalUncalibHitWeights.EEdigiCollection = 'ecalEBunpacker:eeDigis'
-
-# get uncalibrechits with fit method
+    
+# get uncalibrechits with alphabeta fit method
 import RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi
 process.ecalUncalibHitFixedAlphaBetaFit = RecoLocalCalo.EcalRecProducers.ecalFixedAlphaBetaFitUncalibRecHit_cfi.ecalFixedAlphaBetaFitUncalibRecHit.clone()
-process.ecalUncalibHitFixedAlphaBetaFit.EBdigiCollection = 'ecalEBunpacker:ebDigis'
-process.ecalUncalibHitFixedAlphaBetaFit.EEdigiCollection = 'ecalEBunpacker:eeDigis'
 
-# get uncalibrechits with ratio method
-import RecoLocalCalo.EcalRecProducers.ecalRatioUncalibRecHit_cfi
-process.ecalUncalibHitRatio = RecoLocalCalo.EcalRecProducers.ecalRatioUncalibRecHit_cfi.ecalRatioUncalibRecHit.clone()
-process.ecalUncalibHitRatio.EBdigiCollection = 'ecalEBunpacker:ebDigis'
-process.ecalUncalibHitRatio.EEdigiCollection = 'ecalEBunpacker:eeDigis'
+# get uncalibrechits with analytic fit method  
+import RecoLocalCalo.EcalRecProducers.ecalAnalFitUncalibRecHit_cfi
+process.ecalUncalibHitAnalFit = RecoLocalCalo.EcalRecProducers.ecalAnalFitUncalibRecHit_cfi.ecalAnalFitUncalibRecHit.clone()
+
+# get uncalibrechits with ratio method (does not run in 5_3_12)
+#import RecoLocalCalo.EcalRecProducers.ecalRatioUncalibRecHit_cfi
+#process.ecalUncalibHitRatio = RecoLocalCalo.EcalRecProducers.ecalRatioUncalibRecHit_cfi.ecalRatioUncalibRecHit.clone()
+#process.ecalUncalibHitRatio.EBdigiCollection = 'ecalEBunpacker:ebDigis'
+#process.ecalUncalibHitRatio.EEdigiCollection = 'ecalEBunpacker:eeDigis'
 
 
 # get uncalibrechits with ratio method
 import RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi
 process.ecalUncalibHitGlobal = RecoLocalCalo.EcalRecProducers.ecalGlobalUncalibRecHit_cfi.ecalGlobalUncalibRecHit.clone()
-process.ecalUncalibHitGlobal.EBdigiCollection = 'ecalEBunpacker:ebDigis'
-process.ecalUncalibHitGlobal.EEdigiCollection = 'ecalEBunpacker:eeDigis'
 
+# get the recovered digis
+if isMC:
+    process.ecalDetIdToBeRecovered.ebSrFlagCollection = 'simEcalDigis:ebSrFlags'
+    process.ecalDetIdToBeRecovered.eeSrFlagCollection = 'simEcalDigis:eeSrFlags'
+    process.ecalRecHit.recoverEBFE = False
+    process.ecalRecHit.recoverEEFE = False
+    process.ecalRecHit.killDeadChannels = False
+if runOnRAW:
+    process.ecalDetIdToBeRecovered.ebIntegrityGainErrors = 'ecalEBunpacker:EcalIntegrityGainErrors'
+    process.ecalDetIdToBeRecovered.ebIntegrityGainSwitchErrors = 'ecalEBunpacker:EcalIntegrityGainSwitchErrors'
+    process.ecalDetIdToBeRecovered.ebIntegrityChIdErrors = 'ecalEBunpacker:EcalIntegrityChIdErrors'
+    process.ecalDetIdToBeRecovered.eeIntegrityGainErrors = 'ecalEBunpacker:EcalIntegrityGainErrors'
+    process.ecalDetIdToBeRecovered.eeIntegrityGainSwitchErrors = 'ecalEBunpacker:EcalIntegrityGainSwitchErrors'
+    process.ecalDetIdToBeRecovered.eeIntegrityChIdErrors = 'ecalEBunpacker:EcalIntegrityChIdErrors'
+    process.ecalDetIdToBeRecovered.integrityTTIdErrors = 'ecalEBunpacker:EcalIntegrityTTIdErrors'
+    process.ecalDetIdToBeRecovered.integrityBlockSizeErrors = 'ecalEBunpacker:EcalIntegrityBlockSizeErrors'
+else:
+    process.ecalRecHit.ebDetIdToBeRecovered = ''
+    process.ecalRecHit.eeDetIdToBeRecovered = ''
+    process.ecalRecHit.ebFEToBeRecovered = ''
+    process.ecalRecHit.eeFEToBeRecovered = ''
+
+wrongAlgoMessage = "The AmplitudeRecoMethod: "+AmplitudeRecoMethod+" that you chose is not foreseen. Check the customization of the cfg "
 
 # get rechits e.g. from the weights
 process.load("CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi")
 process.load("RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi")
-process.ecalRecHit.EBuncalibRecHitCollection = 'ecalUncalibHit:EcalUncalibRecHitsEB'
-process.ecalRecHit.EEuncalibRecHitCollection = 'ecalUncalibHit:EcalUncalibRecHitsEE'
+process.ecalRecHit.triggerPrimitiveDigiCollection = 'ecalEBunpacker:EcalTriggerPrimitives'
+if AmplitudeRecoMethod == "Global":
+    print "==> Using Global as amplitude reconstruction"
+    process.ecalRecHit.EBuncalibRecHitCollection = 'ecalUncalibHitGlobal:EcalUncalibRecHitsEB'
+    process.ecalRecHit.EEuncalibRecHitCollection = 'ecalUncalibHitGlobal:EcalUncalibRecHitsEE'
+elif AmplitudeRecoMethod == "Weights":
+    print "==> Using Weights as amplitude reconstruction"
+    process.ecalRecHit.EBuncalibRecHitCollection = 'ecalUncalibHitWeights:EcalUncalibRecHitsEB'
+    process.ecalRecHit.EEuncalibRecHitCollection = 'ecalUncalibHitWeights:EcalUncalibRecHitsEE'
+elif AmplitudeRecoMethod == "AlphaBetaFit":
+    print "==> Using AlphaBetaFit as amplitude reconstruction" 
+    process.ecalRecHit.EBuncalibRecHitCollection = 'ecalUncalibHitFixedAlphaBetaFit:EcalUncalibRecHitsEB'
+    process.ecalRecHit.EEuncalibRecHitCollection = 'ecalUncalibHitFixedAlphaBetaFit:EcalUncalibRecHitsEE'
+elif AmplitudeRecoMethod == "AnalyticFit":
+    print "==> Using AnalyticFit as amplitude reconstruction" 
+    process.ecalRecHit.EBuncalibRecHitCollection = 'ecalUncalibHitAnalFit:EcalUncalibRecHitsEB'
+    process.ecalRecHit.EEuncalibRecHitCollection = 'ecalUncalibHitAnalFit:EcalUncalibRecHitsEE'        
+else: print wrongAlgoMessage
 
-
-process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(1000) )
 process.source = cms.Source("PoolSource",
-            fileNames = cms.untracked.vstring('/store/data/Commissioning08/Cosmics/RAW/v1/000/067/838/006945C8-40A5-DD11-BD7E-001617DBD556.root')
-            #fileNames = cms.untracked.vstring('file:/data/franzoni/data/Commissioning08_RAW_v1_67838-006945C8-40A5-DD11-BD7E-001617DBD556.root')
-            #fileNames = cms.untracked.vstring('file:/data/franzoni/data/h4b-sm6/h4b.00016428.A.0.0.root')
-                )
+             fileNames = cms.untracked.vstring('/store/cmst3/user/bendavid/photongun_nopu/photongun_nopu_1000_1_POT.root')
+                ) 
 
 
-process.outputmodule = cms.OutputModule("PoolOutputModule",
-                                          outputCommands = cms.untracked.vstring(
-            'drop *',
-            'keep *_ecalUncalibHit*_*_*',
-            'keep *_ecalRecHit_*_*'
-          ),
-  fileName = cms.untracked.string('testEcalLocalRecoA.root')
-)
+process.out = cms.OutputModule("PoolOutputModule",
+                               outputCommands = cms.untracked.vstring('drop *',
+                                                                      'keep *_ecalUncalibHit*_*_testEcalRecoLocal',
+                                                                      'keep *_ecalRecHit_*_testEcalRecoLocal',
+                                                                      'keep *_offlineBeamSpot_*_*',
+                                                                      'keep *_addPileupInfo_*_*'
+                                                                      ),
+                               fileName = cms.untracked.string('testEcalLocalRecoA.root')
+                               )
 
-process.dumpEv = cms.EDAnalyzer("EventContentAnalyzer")
 
-process.ecalTestRecoLocal = cms.Sequence(process.ecalEBunpacker
-                                         *process.ecalUncalibHitWeights
-                                         *process.ecalUncalibHitFixedAlphaBetaFit
-                                         *process.ecalUncalibHitRatio
+
+if AmplitudeRecoMethod == "Global":              process.ecalAmplitudeReco = cms.Sequence(process.ecalUncalibHitGlobal)
+elif AmplitudeRecoMethod == "Weights":           process.ecalAmplitudeReco = cms.Sequence(process.ecalUncalibHitWeights)
+elif AmplitudeRecoMethod == "AlphaBetaFit":      process.ecalAmplitudeReco = cms.Sequence(process.ecalUncalibHitFixedAlphaBetaFit)
+elif AmplitudeRecoMethod == "AnalyticFit":       process.ecalAmplitudeReco = cms.Sequence(process.ecalUncalibHitAnalFit)
+else: print wrongAlgoMessage
+
+process.ecalTestRecoLocal = cms.Sequence(#process.ecalEBunpacker
+                                         process.ecalAmplitudeReco
                                          *process.ecalUncalibHitGlobal
+#                                         *process.ecalDetIdToBeRecovered
                                          *process.ecalRecHit
-                                         *process.outputmodule
-                                         #*process.dumpEv
                                         )
 
+from PhysicsTools.PatAlgos.tools.helpers import *
+#if isMC:
+#     massSearchReplaceAnyInputTag(process.ecalTestRecoLocal,cms.InputTag("ecalDigis:ebDigis"), cms.InputTag("simEcalDigis:ebDigis"),True)
+#     massSearchReplaceAnyInputTag(process.ecalTestRecoLocal,cms.InputTag("ecalDigis:eeDigis"), cms.InputTag("simEcalDigis:eeDigis"),True)
+if runOnRAW:
+    massSearchReplaceAnyInputTag(process.ecalTestRecoLocal,cms.InputTag("ecalDigis:ebDigis"), cms.InputTag("ecalEBunpacker:ebDigis"),True)
+    massSearchReplaceAnyInputTag(process.ecalTestRecoLocal,cms.InputTag("ecalDigis:eeDigis"), cms.InputTag("ecalEBunpacker:eeDigis"),True)
+
 process.p = cms.Path(process.ecalTestRecoLocal)
+process.outpath = cms.EndPath(process.out)
+process.GlobalTag.globaltag = 'START71_V1::All'

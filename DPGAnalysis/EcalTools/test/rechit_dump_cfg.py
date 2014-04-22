acc6419d94986f04b4266a28d1a3cc64fb455a8f
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-04-22 10:47:04 +0200

    add to this branch

---
+++ DPGAnalysis/EcalTools/test/rechit_dump_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+# configure here #
+isMC = True
+##################
+
+process = cms.Process("ECALRecHitAnalysis")
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load('Configuration.Geometry.GeometryExtended2017Reco_cff')
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+if isMC:
+    process.GlobalTag.globaltag = 'DES17_61_V5::All'
+    #inputfile =  cms.untracked.vstring('/store/group/phys_egamma/emanuele/ecal/reconstruction/ZEE_14TeV_PU140_DR61SLHCx_GENSIMRECO.root')
+    inputfile = cms.untracked.vstring('file:testEcalLocalRecoA.root')
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+process.source = cms.Source("PoolSource",
+                            noEventSort = cms.untracked.bool(True),
+                            duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+                            fileNames = inputfile
+                            )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.load("DPGAnalysis.EcalTools.rechit_dumper_cfi")
+
+process.mypath = cms.Path(process.rechitDumper)

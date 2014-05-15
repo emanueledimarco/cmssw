e45ac6b5954c3ad86bf83b3ef9bdff4573f892b8
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-05-15 19:08:33 +0200

    last vrsion, dumping and analyzing an ntuple with everything

--- DPGAnalysis/EcalTools/test/rechit_dump_cfg.py
+++ DPGAnalysis/EcalTools/test/rechit_dump_cfg.py
@@ -15,20 +15,23 @@ process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 if isMC:
     process.GlobalTag.globaltag = 'START71_V1::All'
-    #inputfile =  cms.untracked.vstring('/store/group/phys_egamma/emanuele/ecal/reconstruction/ZEE_14TeV_PU140_DR61SLHCx_GENSIMRECO.root')
-    inputfile = cms.untracked.vstring('file:testEcalLocalRecoA.root')
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.source = cms.Source("PoolSource",
                             noEventSort = cms.untracked.bool(True),
                             duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
-                            fileNames = inputfile
+                            fileNames = cms.untracked.vstring('file:testEcalLocalRecoA.root')
                             )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.load("DPGAnalysis.EcalTools.rechit_dumper_cfi")
+process.rechitDumper.EBRecHits1 = "ecalRecHitGlobal:EcalRecHitsGlobalEB"
+process.rechitDumper.EERecHits1 = "ecalRecHitGlobal:EcalRecHitsGlobalEE"
+process.rechitDumper.EBRecHits2 = "ecalRecHitWeights:EcalRecHitsWeightsEB"
+process.rechitDumper.EERecHits2 = "ecalRecHitWeights:EcalRecHitsWeightsEE"
+process.rechitDumper.fileName = cms.untracked.string('tree_ecalhits.root')
 
 process.mypath = cms.Path(process.rechitDumper)

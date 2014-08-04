3b76b48b7f53acc3068e4eadb36dd604cfe558a0
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-08-04 11:07:29 +0200

    simply store histograms to afs

--- DPGAnalysis/EcalTools/macro/submit_batch_ntuple.py
+++ DPGAnalysis/EcalTools/macro/submit_batch_ntuple.py
@@ -21,7 +21,7 @@ def main():
     parser.add_option('-n', '--nfileperjob', action='store',     dest='nfileperjob', help='split the jobs with n files read/batch job'                , default=10,   type='int')
     parser.add_option('-p', '--prefix',      action='store',     dest='prefix',      help='the prefix to be added to the output'                      , default=defaultoutputdir)
     parser.add_option('-a', '--application', action='store',     dest='application', help='the executable to be run'                                  , default='ecalhitana')
-    parser.add_option('-d', '--download',    action='store',     dest='download',    help='download the output on a local computer'                   , default='pccmsrm')
+    parser.add_option('-d', '--download',    action='store',     dest='download',    help='download the output on a local computer'                   , default='')
     parser.add_option('-c', '--create',      action='store_true',dest='create',      help='create only the jobs, do not submit them'                  , default=False)
     parser.add_option('-t', '--testnjobs',   action='store',     dest='testnjobs',   help='submit only the first n jobs'                              , default=1000000, type='int')
     (opt, args) = parser.parse_args()
@@ -36,25 +36,28 @@ def main():
 
     print "the outputs will be in the directory: "+opt.prefix
 
+    #look for the current directory
+    #######################################
+    pwd = os.environ['PWD']
+    scramarch = os.environ['SCRAM_ARCH']
+    #######################################
+
     if opt.download=='pccmsrm':
         diskoutputdir = "/cmsrm/pc24_2/emanuele/data/EcalReco7.1.X/"
     elif opt.download=='cmsphys06':
         diskoutputdir = "/data1/emanuele/EcalRecoPu7.1.0/"
+    else:
+        diskoutputdir = pwd
     diskoutputmain = diskoutputdir+"/"+opt.prefix+"/"+output
 
     os.system("mkdir -p "+opt.prefix+"/"+output)
     os.system("mkdir -p "+opt.prefix+"/"+output+"/log/")
     os.system("mkdir -p "+opt.prefix+"/"+output+"/input/")
     os.system("mkdir -p "+opt.prefix+"/"+output+"/src/")
-    outputroot = diskoutputmain+"/root/"
 
     if (diskoutputdir != "none" and opt.download!=''): 
         os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm24 mkdir -p "+diskoutputmain)
 
-    #look for the current directory
-    #######################################
-    pwd = os.environ['PWD']
-    #######################################
     inputListfile=open(inputlist)
     inputfiles = inputListfile.readlines()
     ijob=0
@@ -73,12 +76,13 @@ def main():
         outputname = opt.prefix+"/"+output+"/src/submit_"+str(ijob)+".src"
         outputfile = open(outputname,'w')
         outputfile.write('#!/bin/bash\n')
-        outputfile.write('export SCRAM_ARCH=slc5_amd64_gcc481\n')
+        outputfile.write('export SCRAM_ARCH='+scramarch+'\n')
         outputfile.write('cd '+pwd+'\n')
         outputfile.write('eval `scramv1 runtime -sh`\n')
         outputfile.write('cd $WORKDIR\n')
         outputfile.write(pwd+'/'+opt.application+' '+inputfilename+" "+output+"_"+str(ijob)+".root \n")
-        if(opt.download!=''): outputfile.write('ls *.root *.txt | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm24:'+diskoutputmain+'/{}\n') 
+        if(opt.download=='pccmsrm'): outputfile.write('ls *.root *.txt | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm24:'+diskoutputmain+'/{}\n') 
+        else: outputfile.write('ls *.root | xargs -i cp {} '+diskoutputmain+'\n')
         outputfile.close
         logfile = opt.prefix+"/"+output+"/log/"+output+"_"+str(ijob)+".log"
         os.system("echo bsub -q "+opt.queue+" -o "+logfile+" source "+pwd+"/"+outputname)

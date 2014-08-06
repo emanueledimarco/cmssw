02bdd21becdc2b66fcc086c474c8acc8d5edc6b7
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2014-08-06 15:59:57 +0200

    force the CMG priorities

--- DPGAnalysis/EcalTools/test/submit_batch.py
+++ DPGAnalysis/EcalTools/test/submit_batch.py
@@ -99,9 +99,9 @@ def main():
         if(opt.eos!=''): outputfile.write('ls *.root | xargs -i xrdcp {} root://eoscms/'+opt.eos+'/\n')
         outputfile.close
         logfile = opt.prefix+"/"+output+"/log/"+output+"_"+str(ijob)+".log"
-        os.system("echo bsub -q "+opt.queue+" -o "+logfile+" source "+pwd+"/"+outputname)
+        os.system("echo bsub -G CMS_CERN01_YODA -q "+opt.queue+" -o "+logfile+" source "+pwd+"/"+outputname)
         if(opt.create==False):
-            os.system("bsub -q "+opt.queue+" -o "+logfile+" source "+pwd+"/"+outputname)
+            os.system("bsub -G CMS_CERN01_YODA -q "+opt.queue+" -o "+logfile+" source "+pwd+"/"+outputname)
         ijob = ijob+1
         if(ijob==opt.testnjobs): break
         # take some sleep after many jobs

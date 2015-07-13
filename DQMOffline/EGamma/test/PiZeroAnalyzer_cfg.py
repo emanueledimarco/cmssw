baa95843e153e41c7955913f35ab62f777cfbc5d
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2015-07-13 12:39:47 +0200

    run from the stream

--- DQMOffline/EGamma/test/PiZeroAnalyzer_cfg.py
+++ DQMOffline/EGamma/test/PiZeroAnalyzer_cfg.py
@@ -4,21 +4,28 @@ process = cms.Process("piZeroAnalysis")
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 process.load("DQMOffline.EGamma.piZeroAnalyzer_cfi")
 process.load("DQMServices.Components.MEtoEDMConverter_cff")
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
 
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
 
 DQMStore = cms.Service("DQMStore")
 
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(100000)
 )
 
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
 
-        '/store/relval/CMSSW_3_6_0_pre2/RelValSingleGammaPt35/GEN-SIM-RECO/MC_3XY_V24-v1/0001/364E7B38-6F27-DF11-91A9-0026189438D4.root',
-        '/store/relval/CMSSW_3_6_0_pre2/RelValSingleGammaPt35/GEN-SIM-RECO/MC_3XY_V24-v1/0000/48AE643B-0727-DF11-99FB-001731AF66F5.root'
+        '/store/data/Run2015B/AlCaP0/RAW/v1/000/251/244/00000/86E14A79-AE25-E511-936E-02163E01207C.root'
+#        '/store/relval/CMSSW_3_6_0_pre2/RelValSingleGammaPt35/GEN-SIM-RECO/MC_3XY_V24-v1/0001/364E7B38-6F27-DF11-91A9-0026189438D4.root',
+#       '/store/relval/CMSSW_3_6_0_pre2/RelValSingleGammaPt35/GEN-SIM-RECO/MC_3XY_V24-v1/0000/48AE643B-0727-DF11-99FB-001731AF66F5.root'
 
 
        
@@ -42,6 +49,49 @@ process.FEVT = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('photonsMEtoEDMConverter.root')
 )
 
+# start from RAW format for more flexibility
+#process.raw2digi_step = cms.Sequence(process.RawToDigi)
+
+#DUMMY RECHIT
+process.dummyHits = cms.EDProducer('DummyRechitDigis',
+                                     doDigi = cms.untracked.bool(True),
+                                     # rechits
+                                     barrelHitProducer      = cms.InputTag('hltAlCaPi0EBUncalibrator','pi0EcalRecHitsEB' ,'HLT'),
+                                     endcapHitProducer      = cms.InputTag('hltAlCaPi0EEUncalibrator','pi0EcalRecHitsEE' ,'HLT'),
+                                     barrelRecHitCollection = cms.untracked.string('dummyBarrelRechits'),
+                                     endcapRecHitCollection = cms.untracked.string('dummyEndcapRechits'),
+                                     # digis
+                                     barrelDigis            = cms.InputTag("hltAlCaPi0EBRechitsToDigis","pi0EBDigis","HLT"),
+                                     endcapDigis            = cms.InputTag("hltAlCaPi0EERechitsToDigis","pi0EEDigis","HLT"),
+                                     barrelDigiCollection   = cms.untracked.string('dummyBarrelDigis'),
+                                     endcapDigiCollection   = cms.untracked.string('dummyEndcapDigis'))
+
+# ECAL reco
+import RecoLocalCalo.EcalRecProducers.ecalMultiFitUncalibRecHit_cfi
+process.ecalMultiFitUncalibRecHit =  RecoLocalCalo.EcalRecProducers.ecalMultiFitUncalibRecHit_cfi.ecalMultiFitUncalibRecHit.clone()
+process.ecalMultiFitUncalibRecHit.EBdigiCollection = cms.InputTag('dummyHits','dummyBarrelDigis','piZeroAnalysis')
+process.ecalMultiFitUncalibRecHit.EEdigiCollection = cms.InputTag('dummyHits','dummyEndcapDigis','piZeroAnalysis')
+
+#UNCALIB to CALIB
+from RecoLocalCalo.EcalRecProducers.ecalRecHit_cfi import *
+process.ecalDetIdToBeRecovered =  RecoLocalCalo.EcalRecProducers.ecalDetIdToBeRecovered_cfi.ecalDetIdToBeRecovered.clone()
+process.ecalRecHit.killDeadChannels = cms.bool( False )
+process.ecalRecHit.recoverEBVFE = cms.bool( False )
+process.ecalRecHit.recoverEEVFE = cms.bool( False )
+process.ecalRecHit.recoverEBFE = cms.bool( False )
+process.ecalRecHit.recoverEEFE = cms.bool( False )
+process.ecalRecHit.recoverEEIsolatedChannels = cms.bool( False )
+process.ecalRecHit.recoverEBIsolatedChannels = cms.bool( False )
+
+process.ecalReco = cms.Sequence(process.dummyHits
+                                * process.ecalMultiFitUncalibRecHit 
+                                * process.ecalRecHit )
+
+#process.options = cms.untracked.PSet(
+#   wantSummary = cms.untracked.bool(True),
+#   SkipEvent = cms.untracked.vstring('ProductNotFound','CrystalIDError','CrystalIDError2')
+#)
+
 from DQMOffline.EGamma.piZeroAnalyzer_cfi import *
 piZeroAnalysis.OutputMEsInRootFile = cms.bool(True)
 piZeroAnalysis.OutputFileName = 'DQMPiZeros.root'
@@ -49,9 +99,11 @@ piZeroAnalysis.Verbosity = cms.untracked.int32(0)
 piZeroAnalysis.standAlone = cms.bool(True)
 
 
+process.load('DQMServices.Components.DQMFileSaver_cfi')
+process.dqmSaver.workflow = '/Egamma/PiZeroAnalyzer/All'
 
 #process.p1 = cms.Path(process.MEtoEDMConverter)
 #process.p1 = cms.Path(process.piZeroAnalysis*process.MEtoEDMConverter)
-process.p1 = cms.Path(process.piZeroAnalysis)
+process.p1 = cms.Path(process.ecalReco * process.piZeroAnalysis * process.dqmSaver)
 process.schedule = cms.Schedule(process.p1)
 

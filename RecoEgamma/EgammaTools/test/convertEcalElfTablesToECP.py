6430259c132ac478df65eb5364a05ad3c08e8c1b
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-09-26 14:16:49 +0200

    add a script to convert ECALELF old txt files to ECP txt files, input of CMSSW DB reader

---
+++ RecoEgamma/EgammaTools/test/convertEcalElfTablesToECP.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# usage: python convertEcalElfTablesToECP.py Moriond17_23Jan_ele_scales.dat test
+import sys, os.path
+
+infile, outfile = sys.argv[1], sys.argv[2]
+outf = {'EB':open(outfile+"EB.txt",'w'), 'EE':open(outfile+"EE.txt",'w')}
+lines = {'EB':0,'EE':0}
+for line in open(infile,'r').readlines():
+    fields = line.split()
+    bins = fields[0].split('-')
+    ranges = {}
+    for b in bins:
+        binvar = b.split('_')[0]
+        if any(var in binvar for var in ['absEta','Et']):
+            ranges[binvar] = b.split('_')[1:]
+        elif 'bad' in binvar:
+            ranges['r9'] = ['-1','0.94']
+        elif 'gold' in binvar:
+            ranges['r9'] = ['0.94','2']
+        elif 'gainEle' in binvar:
+            ranges['gain'] = [float(b.split('_')[1])-0.5,float(b.split('_')[1])+0.5]
+        else: print "ERROR! binvar ",binvar," unknown. Not adding it."
+    pars = fields[4:]
+    variables=ranges.keys()
+    variables.sort()
+    subdet = 'EB' if float(ranges['absEta'][0])<1.5 else 'EE'
+    if lines[subdet]==0:
+        outf[subdet].write(('{%d' % len(bins))+''.join(['%20s' % v for v in variables])+
+                           ('%10d' % len(pars))+'          '+'         '.join(['par_%d' % p for p in xrange(len(pars))])+'}\n')
+    for v in variables:
+        r = ranges[v]
+        outf[subdet].write('%10s%10s' % (r[0],r[1])+'    ')
+    outf[subdet].write(''.join(['%10s' % p for p in pars])+'\n')
+    lines[subdet] += 1
+
+print "DONE. Output written into ",outfile

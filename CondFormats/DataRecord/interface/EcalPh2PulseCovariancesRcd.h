b99ea323c7af458bd8dd3fba2981f2c34c82f873
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2026-04-14 19:31:24 +0200

    use better naming convention for phase2 multifit pulse shapes inputs

--- CondFormats/DataRecord/interface/EcalPh2PulseCovariancesRcd.h
+++
@@ -1,7 +0,0 @@
-#ifndef CondFormats_DataRecord_EcalPh2PulseCovariancesRcd_h
-#define CondFormats_DataRecord_EcalPh2PulseCovariancesRcd_h
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class EcalPh2PulseCovariancesRcd : public edm::eventsetup::EventSetupRecordImplementation<EcalPh2PulseCovariancesRcd> {
-};
-#endif
